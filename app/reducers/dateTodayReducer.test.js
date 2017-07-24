968a08a42c5e75f0b926bf439f8af7b57e591aa0
Evan Miller <evanmillerny@gmail.com>
2017-07-23 19:20:34 -0600

    add testing for reducers

---
+++ app/reducers/dateTodayReducer.test.js
@@ -0,0 +1,15 @@
+import { setDateTodayData } from './dateTodayReducer';
+
+describe('setDateTodayData  reducer', () => {
+  it('Should return an initial state', () => {
+    expect(setDateTodayData(undefined, {})).toEqual([]);
+  });
+
+  it('Should return an updated state if given an action', () => {
+    const emptyState = setDateTodayData(undefined, {});
+    const dataState = setDateTodayData(["Sunday", "5:38:18 pm"], {type: 'DATE_TODAY_DATA'});
+
+    expect(emptyState).toEqual([]);
+    expect(dataState).toEqual(undefined);
+  });
+});
---
+++ app/reducers/weatherReducer.test.js
@@ -0,0 +1,17 @@
+import { setWeatherData } from './weatherReducer';
+
+describe('setWeatherData  reducer', () => {
+  it('Should return an initial state', () => {
+    expect(setWeatherData(undefined, {})).toEqual([]);
+  });
+
+  it('Should return an updated state if given an action', () => {
+    const darkSkyData = {city: "Denver", icon: "PARTLY_CLOUDY_DAY", temp: 88}
+
+    const emptyState = setWeatherData(undefined, {});
+    const dataState = setWeatherData([], {type: "WEATHER_DATA", weatherData: darkSkyData})
+
+    expect(emptyState).toEqual([]);
+    expect(dataState).toEqual(darkSkyData);
+  });
+});
