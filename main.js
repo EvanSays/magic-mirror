f984541944fa6ec44f7f9f72cbdd6b018fce4fac
EvanSays <evanmillerny@gmail.com>
2017-07-25 20:20:29 +0000

    add frameless to work on pi, add css changes to remove scroll bars

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({frame: false, webPreferences: { experimentalFeatures: true, webSecurity: false } })
+  win = new BrowserWindow({webPreferences: { experimentalFeatures: true, webSecurity: false }, frame: false, titleBarStyle: 'hidden'})
   win.maximize()
+  win.setFullScreen(true)
   win.show()
 
   // and load the index.html of the app.
